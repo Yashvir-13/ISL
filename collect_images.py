1e27ee01e265077a488ef62eaf85d2b2e88df254
yashvir13112004 <yashvir13112004@example.com>
2024-08-31 12:08:25 +0530

    Update files with latest changes

--- collect_images.py
+++ collect_images.py
@@ -6,7 +6,7 @@ if not os.path.exists(data_dir):
 num_classes=35
 dataset_size=200
 cap=cv2.VideoCapture(0)
-for j in range(10,19+1):
+for j in range(10,15+1):
     if not os.path.exists(os.path.join(data_dir,str(j))):
         os.makedirs(os.path.join(data_dir,str(j)))
     print("Collecting data for class{}".format(j))
